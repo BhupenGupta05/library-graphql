fa00b85f4ce839afb41e57c63220e92f7b76d3f7
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2024-02-02 20:36:49 +0530

    Implemented dropdown instead of typing author's name

--- src/components/UpdateAuthor.jsx
+++ src/components/UpdateAuthor.jsx
@@ -1,35 +1,59 @@
 import { useState } from "react"
 import { EDIT_AUTHOR, ALL_AUTHORS } from "../queries"
-import { useMutation } from "@apollo/client"
+import { useMutation, useQuery } from "@apollo/client"
+import Select from "react-select"
 
 const UpdateAuthor = () => {
+  const authorsResult = useQuery(ALL_AUTHORS)
+
     const [name, setName] = useState('')
     const [born, setBorn] = useState('')
+    const [selectedAuthor, setSelectedAuthor] = useState('')
     const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
         refetchQueries: [ {query: ALL_AUTHORS} ]
       })
 
+      const options =
+      authorsResult.data && authorsResult.data.allAuthors
+      ? authorsResult.data.allAuthors.map((author) => (
+          <option key={author.name} value={author.name}>
+            {author.name}
+          </option>
+        ))
+      : []
+
     const submit = async (e) => {
         e.preventDefault()
 
-        editAuthor({ variables: {name, setBornTo: parseInt(born)} })
+        if(!selectedAuthor) {
+          return;
+        }
+
+        editAuthor({ variables: {name: selectedAuthor.value, setBornTo: parseInt(born)} })
 
         setName('')
         setBorn('')
+        setSelectedAuthor(null)
     }
   return (
     <div>
         <h2>set birthyear</h2>
         <form onSubmit={submit}>
+
         <div>
-          name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          <label>Select author: </label>
+          <select
+            value={selectedAuthor}
+            onChange={(e) => setSelectedAuthor(e.target.value)}>
+              <option value="" disabled>
+                Select an author
+                </option>
+                {options}
+          </select>
         </div>
+
         <div>
-          born
+        <label>Born: </label>
           <input
             type="number"
             value={born}
